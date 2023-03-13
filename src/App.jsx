e201fb2a12513f6ce55e79736c94eaa5604911c6
Abhiraj-cuchd <21bca1058@cuchd.in>
2023-03-14 03:28:34 +0530

    progress commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchData } from './Utils/api';
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from "./Pages/Home/Home";
+import Error404 from './Pages/404/Error404';
+import Explore from './Pages/Explore/Explore'
+import Details from './Pages/Details/Details';
+import SearchResult from './Pages/SearchResult/searchResult';
+
+
 
 const App = () => {
 
+    const dispatch = useDispatch();
+
+    const { url } = useSelector((state) => state.home)
+    console.log(url);
+
     useEffect(() => {
-        testApi();
+        fetchApiConfig();
     }, [])
 
-    const testApi = () => {
-        fetchData('/movie/popular?api_key=')
+    const fetchApiConfig = () => {
+        fetchData('/configuration?api_key=')
             .then((res) => {
                 console.log(res);
+
+                const url = {
+                    backdrop: res.images.secure_base_url + "original",
+                    poster: res.images.secure_base_url + "original",
+                    profile: res.images.secure_base_url + "original",
+                }
+
+                dispatch(getApiConfiguration(url));
             });
     }
 
     return (
-        <div className="App">
-            <h1>Cinematix</h1>
-        </div>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} exact />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+            {/* <Footer /> */}
+        </BrowserRouter>
     )
 }
 
---
+++ src/Pages/404/Error404.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const Error404 = () => {
+  return (
+    <div>404</div>
+  )
+}
+
+export default Error404
